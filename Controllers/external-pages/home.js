ba14556f746ff4272c63ea7b4446b8be1844be86
Orefejo <adepeju.kayode@gmail.com>
2020-07-24 22:23:22 +0200

    hmm

--- Controllers/external-pages/home.js
+++ Controllers/external-pages/home.js
@@ -6,7 +6,7 @@ const home = (req, res) => {
     firstName: 'John',
     lastName: 'Doe'
   }
-  renderPage(res, 'employerDashboard', data, 'Demo Page')
+  renderPage(res, 'employer/employerDashboard', data, 'Demo Page')
 };
 
 module.exports.home = home;
